1d4fb3fe3f4d1a979b468c436919d5620b07b3af
Rakh40 <144672333+Rakh40@users.noreply.github.com>
2023-10-05 10:36:49 +0530

    first commit

---
+++ src/main/java/com_lulu_product/ControllerTester.java
@@ -0,0 +1,46 @@
+package com_lulu_product;
+import java.sql.SQLException;
+public class ControllerTester {
+	
+
+			public static void main(String[] args) throws SQLException {
+
+	 
+
+				// TODO Auto-generated method stub
+
+	
+
+				Product bl= new Product();
+
+				bl.readData();
+
+		     	bl.createData();
+
+	 
+
+				bl.readData();
+
+	 
+
+		     	bl.deletData();
+
+	 
+
+		    	bl.readData();
+
+	 
+
+				bl.updateData();
+
+	 
+
+				bl.readData();
+
+				//bl.tryBatchMore();
+
+			}
+
+		}
+
+	 
